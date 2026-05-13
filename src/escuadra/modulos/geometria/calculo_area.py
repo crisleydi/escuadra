5d4b83523aa0a47f87bfbb60cc34ab79212a8128
Jhaf <floresfernandezjonathanaugusto@gmail.com>
2026-05-12 23:51:36 -0400

    feat(geometría): implementacion de calculo de areas - Closes #138

---
+++ src/escuadra/modulos/geometria/calculo_area.py
@@ -0,0 +1,11 @@
+def area_triangulo(base, altura):
+    return (base * altura) / 2
+
+def area_circulo(radio):
+    return 3.1416 * (radio ** 2)
+
+def area_rectangulo(base, altura):
+    return base * altura
+print("el area del triangulo es:", area_triangulo(2, 4))
+print("el area del circulo es:", area_circulo(4))
+print("el area del rectangulo es:", area_rectangulo(58, 46))
